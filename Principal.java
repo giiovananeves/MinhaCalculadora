da915e08535c12720b087db8da9a9d818e844a02
giiovananeves <giovana.silva23@fatec.sp.gov.br>
2022-04-11 19:26:24 +0000

    resultado

---
+++ Principal.java
@@ -0,0 +1,11 @@
+public class Principal{
+    
+    public static void main(String [] args){
+
+        MinhaCalculadora calc=new MinhaCalculadora();
+
+        System.out.println(calc.soma(38,1));
+
+    }
+
+}
